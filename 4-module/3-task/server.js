b2ffec959bdb5aa722086b2526cceed3e74053bd
Konstantin Skok <skok.konstantin@gmail.com>
2020-01-31 00:49:06 +0300

    add solution for 4-module 3-task

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,22 +1,43 @@
-const url = require('url');
-const http = require('http');
-const path = require('path');
+const url = require("url");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
 const server = new http.Server();
 
-server.on('request', (req, res) => {
+server.on("request", (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
 
-  const filepath = path.join(__dirname, 'files', pathname);
+  const pulicDir = path.join(__dirname, "files");
+  const filepath = path.join(pulicDir, pathname);
+
+  if (path.parse(filepath).dir !== pulicDir) {
+    res.statusCode = 400;
+    res.end("Bad path");
+    return;
+  }
 
   switch (req.method) {
-    case 'DELETE':
+    case "DELETE":
+      fs.unlink(filepath, err => {
+        if (err) {
+          if (err.code === "ENOENT") {
+            res.statusCode = 404;
+            res.end();
+          } else {
+            res.statusCode = 500;
+            res.end();
+          }
+        } else {
+          res.end();
+        }
+      });
 
       break;
 
     default:
       res.statusCode = 501;
-      res.end('Not implemented');
+      res.end("Not implemented");
   }
 });
 
