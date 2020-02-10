8af33d6e2268eec8c6748faca84f7b79a5369158
Konstantin Skok <Skok.K.S@sberbank.ru>
2020-02-10 12:09:54 +0300

    add solution to 5-module 1-task

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -1,19 +1,32 @@
 const Koa = require("koa");
 const app = new Koa();
 
+const Chat = require("./chat");
+
 app.use(require("koa-static")("public"));
 app.use(require("koa-bodyparser")());
 
 const Router = require("koa-router");
 const router = new Router();
 
-// router.get('/', (ctx, next) => {
-//     const indexPage =
-// })
+const chat = new Chat();
 
-router.get("/subscribe", async (ctx, next) => {});
+router.get("/subscribe", async (ctx, next) => {
+  const message = await new Promise((resolve, reject) => {
+    chat.subscribe(resolve);
+  });
+  ctx.body = message;
+});
 
-router.post("/publish", async (ctx, next) => {});
+router.post("/publish", async (ctx, next) => {
+  const message = ctx.request.body.message;
+  if (!message || !message.trim()) {
+    ctx.body = "";
+    return;
+  }
+  chat.publish(message);
+  ctx.body = "";
+});
 
 app.use(router.routes());
 
